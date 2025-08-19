f6568227f0c93b10c6dbaa43013920ed80233ef0
Mayank jangid <mayankjangid0007@gmail.com>
2025-08-19 12:48:10 +0530

    chore: set up tRPC with initial configuration

--- src/modules/home/ui/views/home-views.tsx
+++ src/modules/home/ui/views/home-views.tsx
@@ -1,43 +1,18 @@
 "use client"
 
-import { Button } from "@/components/ui/button" 
-import { authClient } from "@/lib/auth-client"
-import { useRouter } from "next/navigation"
-import { useEffect, useState } from "react"
- 
+import { useTRPC } from "@/trpc/client"
+import { useQuery } from "@tanstack/react-query"
 
 export function HomeView() {
-  const router = useRouter()
-  const [session, setSession] = useState<any>(null)
-  const [loading, setLoading] = useState(true)
 
-  useEffect(() => {
-    // yahan pe async function bana ke session fetch kar rahe hain
-    const fetchSession = async () => {
-      const { data: session } = await authClient.getSession()
-      setSession(session)
-      setLoading(false)
-    }
-    fetchSession()
-  }, [])
+  const trpc = useTRPC();
+  const { data } = useQuery(trpc.hello.queryOptions({text: "Mayank"}));
 
-  if (loading) {
-    return <h1>Loading....</h1>
-  }
-  if (!session) {
-    return <h1>No session found. Please login.</h1>
-  }
+
 
   return (
-    <div>
-      <h1>Logged in as {session.user.name}</h1>
-      <Button onClick={() => authClient.signOut({
-        fetchOptions: {
-          onSuccess: () => router.push('/sign-in')
-        }
-      })}>
-        Sign Out
-      </Button>
+    <div className="flex flex-col p-4 gap-y-4">
+      {data?.greeting}
     </div>
   )
 }
