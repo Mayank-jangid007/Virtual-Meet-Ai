85390ebe14b860e8768637b3515ab8ca6a939002
Mayank jangid <mayankjangid0007@gmail.com>
2025-09-20 10:41:11 +0530

    meeting-variants

--- src/modules/meetings/ui/view/meeting-id-view.tsx
+++ src/modules/meetings/ui/view/meeting-id-view.tsx
@@ -9,6 +9,10 @@ import { useRouter } from "next/navigation";
 import { useConfirm } from "@/hooks/use-confirm";
 import { UpdateMeetingDialog } from "../components/update-meeting-dialog";
 import { useState } from "react";
+import { UpcomingState } from "../components/upcoming-state";
+import { ActiveState } from "../components/active-state";
+import { CancelledState } from "../components/cancelled-state";
+import { ProcessingState } from "../components/processing-state";
 
 interface Props{
     meetingId: string;
@@ -44,6 +48,12 @@ export const MeetingIdView = ({ meetingId }: Props) =>{
         await removeMeeting.mutateAsync({ id: meetingId })
     }
 
+    const isActive = data.existingMeeting.status == 'ACTIVE'; 
+    const isUpcoming = data.existingMeeting.status == 'UPCOMING'; 
+    const isCancelled = data.existingMeeting.status == 'CANCELLED'; 
+    const isCompleted = data.existingMeeting.status == 'COMPLETED'; 
+    const isProcessing = data.existingMeeting.status == 'PROCESSING'; 
+
     return (
         <>
             <RemoveConfirmation />
@@ -59,7 +69,17 @@ export const MeetingIdView = ({ meetingId }: Props) =>{
                     onEdit={() => setUpdateMeetingDialogOpen(true)}
                     onRemove={handleRemoveMeeting}
                 />
-                {JSON.stringify(data, null, 2)}
+                {isCompleted && <div>Completed</div>}
+                {isProcessing && <ProcessingState />}
+                {isCancelled && <CancelledState />}
+                {isActive && <ActiveState meetingId={meetingId} />}
+                {isUpcoming && (
+                    <UpcomingState 
+                        meetingId="meetingId"
+                        onCancleMeeting={() => {}}
+                        isCancelling={false}
+                    />
+                )}
             </div>
         </>
     )
